9bccaf8e3506e75c1e36d4d5bafede3d8f5eaeb3
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-09-07 15:50:39 +0500

    function declarations & function experssions

--- 02_js_fundamentals/app.js
+++ 02_js_fundamentals/app.js
@@ -17,34 +17,52 @@ console.log(hasDriversLicense); // true
 // FUNCTIONS
 // INTRO
 
-function logger() {
+/* function logger() {
   console.log('My name is Komil');
-}
+} */
 
 // calling / running / invoking function
 /* logger();
 logger();
 logger(); */
 
-function fruitProcessor(apples, oranges) {
-  /* console.log(apples, oranges);
+/* function fruitProcessor(apples, oranges) { */
+/* console.log(apples, oranges);
   const juice = `My juice with ${apples} apples and ${oranges} oranges
   Hmmmm, mazzamiiii!`;
   return juice; */
 
-  // Easy way
-  return `My juice with ${apples} apples and ${oranges} oranges
+// Easy way
+/*   return `My juice with ${apples} apples and ${oranges} oranges
   Hmmmm, mazzamiiii!`;
-}
+} */
 
 /* const myJuice = fruitProcessor(4, 2);
 console.log(myJuice); */
 
-const appleJuice = fruitProcessor(2, 4);
+/* const appleJuice = fruitProcessor(2, 4);
 const orangeJuice = fruitProcessor(1, 5);
 console.log(appleJuice);
-console.log(orangeJuice);
+console.log(orangeJuice); */
 
 // This is also function
 /* const num = Number('17');
 console.log(num); */
+
+// Function Declarations && Function Experssions
+
+// function declaration
+/* function birthYear(birthYear) {
+  return 2035 - birthYear;
+}
+
+const age1 = birthYear(2001);
+// console.log(age1);
+
+// function experssion
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+
+const age2 = calcAge2(2001);
+console.log(age1, age2); */
