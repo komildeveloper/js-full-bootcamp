d4e76113e4b547edff885de8027fe616ce267f62
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2021-09-13 00:46:38 +0500

    intro arrays

--- 02_js_fundamentals/app.js
+++ 02_js_fundamentals/app.js
@@ -118,21 +118,44 @@ console.log(yearsUntilRetirement(2001, 'Komil'));
 console.log(yearsUntilRetirement(1951, 'Sobit'));
  */
 
-///////////////////////////////////////
-// Coding Challenge #1
+// INTRO ARRAYS
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
 
-/*
-Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
-Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
-A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
-1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-2. Use the function to calculate the average for both teams
-3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
-4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
-5. Ignore draws this time.
-TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-HINT: To calculate average of 3 values, add them all together and divide by 3
-HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
-GOOD LUCK 😀
-*/
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(2002, 2001, 1988, 2008);
+console.log(y);
+
+console.log(friends[0]); // 'Michael'
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // 'Peter'
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Komil';
+const komil = [firstName, 'Sobitov', 2037 - 2001, 'developer', friends];
+console.log(komil);
+console.log(komil.length);
+
+// EXERCISE
+const calcAge = birthYear => {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2001, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1])
+];
+console.log(ages);
