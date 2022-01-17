232fa1957c4cc1bce21ca2a63574f214c107bbe4
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-18 00:52:42 +0500

    loops added

--- 02_js_fundamentals/script.js
+++ 02_js_fundamentals/script.js
@@ -440,6 +440,7 @@ for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`)
 } */
 
+/*
 // LOOPING ARRAYS, BREAKING AND CONTINUING
 const komil = [
   'Komil',
@@ -482,4 +483,50 @@ console.log('--- BREAK WITH NUMBER ---');
 for(let i = 0; i < komil.length; i++) {
   if(typeof komil[i] === 'number') break;
   console.log(komil[i], typeof komil[i]);
+} */
+
+/*
+// Looping Backwards and Loops in Loops
+const komil = [
+  'Komil',
+  'Sobitov',
+  2022 - 2001,
+  'programmer',
+  ['Behzod', 'Bekzod', 'Usmon', 'Abdulaziz', 'Sardor', 'Murod'],
+  true
+]
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for(let i = komil.length -1; i >= 0; i--) {
+  console.log(i, komil[i]);
+}
+
+for(let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+  for(let rep = 1; rep < 6; rep++) {
+    console.log(`  Lifting weight repetition ${rep}`);
+  }
+} */
+
+// While loop
+// console.log('---------- FOR LOOP ----------');
+for(let rep = 1; rep <= 10; rep++) {
+  // console.log(`Lifting weights repetition ${rep}`);
+}
+
+// console.log('---------- WHILE LOOP ----------');
+let rep = 1;
+while(rep <= 10) {
+  // console.log(`Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while(dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if(dice === 6) console.log('Loops is about to end...');
 }
