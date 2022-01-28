45ae403306c51a5be525b257d3d57a650e1cf612
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-28 15:45:12 +0500

    rest pattern added

--- 07_data_structures_modern_operators_and_string/script.js
+++ 07_data_structures_modern_operators_and_string/script.js
@@ -39,8 +39,50 @@ const restuarant = {
 
 	orderPasta: function (ing1, ing2, ing3) {
 		console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`)
+	},
+
+	orderPizza: function (mainIngredient, ...otherIngredients) {
+		console.log(mainIngredient)
+		console.log(otherIngredients)
 	}
 }
+
+/*
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]]
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5]
+console.log(a, b, others)
+
+const [pizza, , risotto, ...otherFood] = [
+	...restuarant.mainMenu,
+	...restuarant.starterMenu
+]
+console.log(pizza, risotto, otherFood)
+
+// Objects
+const { sat, ...weekdays } = restuarant.openingHours
+console.log(weekdays)
+
+// 2) Functions
+const add = function (...numbers) {
+	let sum = 0
+	for (let i = 0; i < numbers.length; i++) sum += numbers[i]
+	console.log(sum)
+}
+add(2, 3)
+add(5, 3, 7, 2)
+add(8, 2, 5, 3, 2, 1, 4)
+
+const x = [23, 5, 7]
+add(...x)
+
+restuarant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
+restuarant.orderPizza('mushrooms')
+*/
 
 /*
 // The Spread Operator(...)
