d48aeae8c9821fa0cc144d7f9540f93def814e61
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-01-28 16:02:28 +0500

    short-circuiting added

--- 07_data_structures_modern_operators_and_string/script.js
+++ 07_data_structures_modern_operators_and_string/script.js
@@ -47,6 +47,36 @@ const restuarant = {
 	}
 }
 
+// SHORT CIRCUITING (&& and ||)
+console.log('---- OR ----')
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas')
+console.log('' || 'Jonas')
+console.log(true || 0)
+console.log(undefined || null)
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null)
+
+restuarant.numGuests = 0
+const guests1 = restuarant.numGuests ? restuarant.numGuests : 10
+console.log(guests1)
+
+const guests2 = restuarant.numGuests || 10
+console.log(guests2)
+
+console.log('---- AND ----')
+console.log(0 && 'Komil')
+console.log(7 && 'Komil')
+
+console.log('Hello' && 23 && null && 'Komil')
+
+// Practical example
+if (restuarant.orderPizza) {
+	restuarant.orderPizza('mushrooms', 'spinach')
+}
+
+restuarant.orderPizza && restuarant.orderPizza('mushrooms', 'spinach')
+
 /*
 // REST PATTERN and PARAMETRS 
 // 1) Destructuring
