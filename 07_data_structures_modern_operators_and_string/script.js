9cc7d788c44b3af77808e320fcbf4dfe9457b02e
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-02-08 20:19:08 +0500

    SETS fundamentals

--- 07_data_structures_modern_operators_and_string/script.js
+++ 07_data_structures_modern_operators_and_string/script.js
@@ -48,7 +48,41 @@ const restuarant = {
 	}
 }
 
+/*
 // Sets
+const ordersSet = new Set([
+	'Pasta',
+	'Pizza',
+	'Pizza',
+	'Risotto',
+	'Pasta',
+	'Pizza'
+])
+console.log(ordersSet)
+
+// console.log(new Set('Komil'))
+
+console.log(ordersSet.size)
+console.log(ordersSet.has('Pizza'))
+console.log(ordersSet.has('Bread'))
+ordersSet.add('Garlic Bread')
+ordersSet.add('Garlic Bread')
+ordersSet.delete('Risotto')
+// ordersSet.clear()
+console.log(ordersSet)
+
+for (const order of ordersSet) console.log(order)
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
+const stuffUnique = [...new Set(staff)]
+console.log(stuffUnique)
+console.log(
+	new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+)
+
+console.log(new Set('komilsobitov').size)
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #2
